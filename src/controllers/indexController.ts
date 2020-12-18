027965a5b5358e6b36285d81ace308262049ca3b
vagrant <tomato.iscandy@gmail.com>
2020-12-18 07:51:15 +0000

    Delete: exclude shared directory

--- src/controllers/indexController.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export class IndexController {
-    index = (req: Request, res: Response, next: NextFunction) => {
-        res.send('Index extended.');
-    }
-}
