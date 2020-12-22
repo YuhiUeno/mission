a163868f5474aa7ffaee677ac1c8dbe48dcda0d0
Yushi <tomato.iscandy@gmail.com>
2020-12-22 15:48:22 +0900

    Add: onTabChanged

--- src/views/app.component.ts
+++ src/views/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'views-root',
@@ -7,7 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent {
   navLinks = [
-      {path: '/dashboard', label: 'DASHBOARD'},
-      {path: '/heroes', label: "HEROES"}
+      {path: '/dashboard', label: 'DASHBOARD', index: 0},
+      {path: '/heroes', label: "HEROES", index: 1}
   ]
+
+  constructor(private router: Router) {
+
+  }
+
+  onTabChanged(event: MatTabChangeEvent): void {
+    switch (event.index) {
+      case 0:
+        this.router.navigate([this.navLinks[0].path])
+        break
+      case 1:
+        this.router.navigate([this.navLinks[1].path])
+        break
+    }
+  }
 }
