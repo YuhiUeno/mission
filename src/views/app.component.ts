15b0b4fcb9bd110cf2f6b7074a89ae256491b57e
Yushi <tomato.iscandy@gmail.com>
2020-12-22 16:35:56 +0900

    Add: primary color

--- src/views/app.component.ts
+++ src/views/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTabChangeEvent } from '@angular/material/tabs';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'views-root',
@@ -13,19 +11,4 @@ export class AppComponent {
       {path: '/dashboard', label: 'DASHBOARD', index: 0},
       {path: '/heroes', label: "HEROES", index: 1}
   ]
-
-  constructor(private router: Router) {
-
-  }
-
-  onTabChanged(event: MatTabChangeEvent): void {
-    switch (event.index) {
-      case 0:
-        this.router.navigate([this.navLinks[0].path])
-        break
-      case 1:
-        this.router.navigate([this.navLinks[1].path])
-        break
-    }
-  }
 }
