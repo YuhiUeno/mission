b084b85b6c2635fe3da7728dc723538e0437edb5
Yushi <tomato.iscandy@gmail.com>
2020-12-22 15:03:19 +0900

    Fix: navLinks list

--- src/views/app.component.ts
+++ src/views/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  navLinks = ['/dashboard', '/heroes']
+  navLinks = [
+    {path: '/dashboard', label: 'DASHBOARD'},
+    {path: '/heroes', label: "HEROES"}
+  ]
   activeLink = this.navLinks[0]
 }
