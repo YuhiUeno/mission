f2e163bcbabd547e78d2d9cd49166d5fff563e21
vagrant <tomato.iscandy@gmail.com>
2020-12-23 13:23:22 +0000

    Add: work card component template

--- src/views/app.module.ts
+++ src/views/app.module.ts
@@ -24,7 +24,8 @@ import { MatTabsModule } from '@angular/material/tabs'
 import { MatCardModule } from '@angular/material/card'
 
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { CoverComponent } from './cover/cover.component'
+import { CoverComponent } from './cover/cover.component';
+import { WorkCardComponent } from './work-card/work-card.component'
 
 @NgModule({
   declarations: [
@@ -36,6 +37,7 @@ import { CoverComponent } from './cover/cover.component'
     HeroSearchComponent,
     LogoComponent,
     CoverComponent,
+    WorkCardComponent,
   ],
   imports: [
     BrowserModule,
