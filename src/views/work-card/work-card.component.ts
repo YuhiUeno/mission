cd36f7ebcd040cde4711227547106fdcef003e1b
Yushi <tomato.iscandy@gmail.com>
2020-12-24 00:53:42 +0900

    Update: work detail section

--- src/views/work-card/work-card.component.ts
+++ src/views/work-card/work-card.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work-card.component.scss']
 })
 export class WorkCardComponent implements OnInit {
-
+  work = {
+    title: "Starting over at 8",
+    tags: ["isekai", "tensei", "political", "magic", "majin"],
+    author: "Yushi",
+    descriptoin: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
+    fund: 1234567
+  }
   constructor() { }
 
   ngOnInit(): void {
