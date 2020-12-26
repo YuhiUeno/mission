c795e2e2deb9b6e12fbc90ca1886f84cf543da17
Yushi <tomato.iscandy@gmail.com>
2020-12-26 16:16:08 +0900

    Fix: remove \n to solve not a function problem

--- src/database/heroes/heroes.statics.ts
+++ src/database/heroes/heroes.statics.ts
@@ -1,11 +1,7 @@
 import { IHeroDocument, IHeroModel } from "./heroes.types";
 import { CounterModel } from "../counters/counters.model";
 
-export async function findOneOrCreate(
-    this: IHeroModel,
-    heroId: number,
-    name: string
-): Promise<IHeroDocument> {
+export async function findOneOrCreate(this: IHeroModel, heroId: number, name: string): Promise<IHeroDocument> {
     const record = await this.findOne({ heroId: heroId });
     if (record) {
         return record;
@@ -14,41 +10,26 @@ export async function findOneOrCreate(
     }
 };
 
-export async function findByHeroId(
-    this: IHeroModel,
-    min?: number,
-    max?: number
-): Promise<IHeroDocument[]> {
+export async function findByHeroId(this: IHeroModel, min?: number, max?: number): Promise<IHeroDocument[]> {
     return this.find({ heroId: {$gte: min || 11, $lte: max || Infinity} });
 };
 
-export async function getHeroes(
-    this: IHeroModel
-): Promise<IHeroDocument[]> {
+export async function getHeroes(this: IHeroModel): Promise<IHeroDocument[]> {
     return this.find();
 };
 
-export async function getHero(
-    this: IHeroModel,
-    heroId: number
-): Promise<IHeroDocument> {
+export async function getHero(this: IHeroModel, heroId: number): Promise<IHeroDocument> {
     return this.findOne({heroId: heroId});
 };
 
-export async function updateHeroName(this: IHeroModel,
-    heroId: number,
-    name: string
-): Promise<void> {
+export async function updateHeroName(this: IHeroModel, heroId: number, name: string): Promise<void> {
     await this.updateOne(
         {heroId: heroId}, 
         {$set: {name: name}}
     );
 };
 
-export async function addHero(
-    this: IHeroModel,
-    name: string
-): Promise<void> {
+export async function addHero(this: IHeroModel, name: string): Promise<void> {
     await CounterModel.findOneAndUpdate(
         {key: "heroId"},
         {$inc: {seq: 1}},
@@ -63,9 +44,6 @@ export async function addHero(
     // console.log(seq)
 };
 
-export async function deleteHero(
-    this: IHeroModel,
-    heroId: number
-): Promise<IHeroDocument> {
+export async function deleteHero(this: IHeroModel, heroId: number): Promise<IHeroDocument> {
     return this.findOneAndDelete({heroId: heroId});
 };
