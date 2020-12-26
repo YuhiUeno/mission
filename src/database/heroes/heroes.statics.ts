2d3ff9d09c3f4db4eb69b6357a9ea60efbb271d0
Yushi <tomato.iscandy@gmail.com>
2020-12-26 16:09:26 +0900

    Add: colon in statics

--- src/database/heroes/heroes.statics.ts
+++ src/database/heroes/heroes.statics.ts
@@ -1,18 +1,18 @@
-import { IHeroDocument, IHeroModel } from "./heroes.types"
-import { CounterModel } from "../counters/counters.model"
+import { IHeroDocument, IHeroModel } from "./heroes.types";
+import { CounterModel } from "../counters/counters.model";
 
 export async function findOneOrCreate(
     this: IHeroModel,
     heroId: number,
     name: string
 ): Promise<IHeroDocument> {
-    const record = await this.findOne({ heroId: heroId })
+    const record = await this.findOne({ heroId: heroId });
     if (record) {
         return record
     } else {
         return this.create({ heroId: heroId, name: name })
     }
-}
+};
 
 export async function findByHeroId(
     this: IHeroModel,
@@ -20,23 +20,22 @@ export async function findByHeroId(
     max?: number
 ): Promise<IHeroDocument[]> {
     return this.find({ heroId: {$gte: min || 11, $lte: max || Infinity} })
-}
+};
 
 export async function getHeroes(
     this: IHeroModel
 ): Promise<IHeroDocument[]> {
     return this.find()
-}
+};
 
 export async function getHero(
     this: IHeroModel,
     heroId: number
 ): Promise<IHeroDocument> {
     return this.findOne({heroId: heroId})
-}
+};
 
-export async function updateHeroName(
-    this: IHeroModel,
+export async function updateHeroName(this: IHeroModel,
     heroId: number,
     name: string
 ): Promise<void> {
@@ -44,7 +43,7 @@ export async function updateHeroName(
         {heroId: heroId}, 
         {$set: {name: name}}
     )
-}
+};
 
 export async function addHero(
     this: IHeroModel,
@@ -62,11 +61,11 @@ export async function addHero(
     // const seq = result.seq
     // await this.create({heroId: seq, name: name})
     // console.log(seq)
-}
+};
 
 export async function deleteHero(
     this: IHeroModel,
     heroId: number
 ): Promise<IHeroDocument> {
     return this.findOneAndDelete({heroId: heroId})
-}
+};
