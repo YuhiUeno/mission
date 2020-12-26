3509a304f05e9004ea727c6012a5f3222384beb2
Yushi <tomato.iscandy@gmail.com>
2020-12-26 10:33:23 +0900

    Add: check if default method is working 2

--- src/mongoose.check.ts
+++ src/mongoose.check.ts
@@ -2,8 +2,10 @@ import { connect, disconnect } from "./database/db";
 import { HeroModel } from "./database/heroes/heroes.model";
 
 async function check(id: number) {
+    connect()
     const hero = await HeroModel.findOne({heroId: id})
     console.log(hero)
+    disconnect()
 }
 
 check(14)
