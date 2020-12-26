0815d218a6e510ed58a35aeefe840e471616fef0
Yushi <tomato.iscandy@gmail.com>
2020-12-26 10:21:46 +0900

    Add: db connection method

--- src/mongoose.check.ts
+++ src/mongoose.check.ts
@@ -1,8 +1,11 @@
+import { connect, disconnect } from "./database/db";
 import { HeroModel } from "./database/heroes/heroes.model";
 
 async function check(id: number) {
+    connect()
     const hero = await HeroModel.getHero(id)
     console.log(hero)
+    disconnect()
 }
 
 check(14)
