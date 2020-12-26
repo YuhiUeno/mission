23951edac0cd2bce25444e495fe53070499875a5
Yushi <tomato.iscandy@gmail.com>
2020-12-26 11:55:27 +0900

    Fix: remove await

--- src/mongoose.check.ts
+++ src/mongoose.check.ts
@@ -4,7 +4,7 @@ import { HeroModel } from "./database/heroes/heroes.model";
 async function check() {
     connect()
     const hero = new HeroModel({name: "Narco"})
-    console.log(await hero.getHeroes())
+    console.log(hero.getHeroes())
 
     disconnect()
 }
