42b5e28d3199d877d5db87a261b0ba8c98e9be0e
Yushi <tomato.iscandy@gmail.com>
2020-12-27 12:28:56 +0900

    Fix: comment out jwt

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-app.use(jwt())
+// app.use(jwt())
 // app.use(cors())
 
 app.use('/api/', indexRouter)
